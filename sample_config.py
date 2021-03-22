9bbaa9483cdc7604707ef43500cdda2c2f540783
illusion999 <79356567+illusion999@users.noreply.github.com>
2021-03-22 16:09:12 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
-api_id = 14371
-api_hash = "e46b6c854d2bf58a0"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+api_id = 3277132
+api_hash = "50ad2ffad01bf10049638526009661a8"
+sudo_chat_id = -1001439516143 -1001405741332
+owner_id = 1410616929
 
 
 # Arq Api, don't make changes here
